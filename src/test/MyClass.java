19167df6f72100d5a551e9d9943d35b395e419bf
Mahatesh2005 <mahantesh2005@gmail.com>
2018-08-05 19:55:31 +0530

    'change'

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class MyClass {
 	public static void main(String args[])throws IOException{
-		System.out.println("enter names");
+		System.out.println("enter namess");
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String name = br.readLine();
 		System.out.println("your name is " +name);
